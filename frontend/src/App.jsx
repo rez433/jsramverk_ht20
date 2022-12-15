61567c69810e30ea577f71e053a634a8e8afed39
rez433 <r3zaee@gmail.com>
2022-12-15 10:43:06 +0100

    backend, frontend and testing added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Editor from './pages/Editor'
+import Register from './pages/Register'
+
+function App() {
+  return (
+    <>
+      <Router basename='/~rere20/editor/'>
+        <div className='container'>
+          <Header />
+          {/* some */}
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/editor/:id' element={<Editor />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
