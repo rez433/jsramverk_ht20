665bdcad3edb746b8d6a3a2ddb23161b8545107e
rez433 <r3zaee@gmail.com>
2022-12-16 13:31:37 +0100

    Update docs.test.js

--- backend/__tests__/docs.test.js
+++ backend/__tests__/docs.test.js
@@ -38,7 +38,7 @@ describe('GET /api/docs', () => {
         await request(app).get('/api/docs')
             .expect(200)
             .then( (res) => {
-                console.log('##############   res.body.title: ', res.body, '+', doc.title)
+                console.log('##############   res.body.title: ', res.body.title, '+', doc.title)
                 expect(res.body.title).toBe(doc.title)
 
                 expect(res.body.text).toBe(doc.text)
